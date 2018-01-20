e031e11eb2496df123bf7abeb887d2c3f920fd99
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-01-20 11:30:12 +0530

    Create 3.c

---
+++ set1/3.c
@@ -0,0 +1,16 @@
+#include<stdio.h
+int main()
+{
+char a;
+printf("enter the character");
+scanf("%c",&a);
+if(a='a'||a=='e'||a=='i'||a=='o'||a=='u')
+{
+printf("vowel");
+}
+else
+{
+printf("constant");
+}
+return 0;
+}
