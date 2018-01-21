7b3c1e89809f72c34e5ed7e58b2300ee26deca3a
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-01-21 16:46:01 +0530

    Update 43.c

--- set5/43.c
+++ set5/43.c
@@ -2,17 +2,17 @@
 int main()
 {
   char s1[25],s2[25];
-  int i=0,j=0;
+  int i=0,k=0;
   printf("\nEnter First String:");
   gets(s1);
   printf("\nEnter Second String:");
   gets(s2);
   while(s1[i]!='\0')
   i++;
-  while(s2[j]!='\0')
+  while(s2[k]!='\0')
   {
-    s1[i]=s2[j];
-    j++;
+    s1[i]=s2[k];
+    k++;
     i++;
   }
   s1[i]='\0';
