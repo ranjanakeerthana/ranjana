238342d6c08c84741d4869c66a4236e1e61356b2
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-01-21 13:55:44 +0530

    Create 15.c

---
+++ set2/15.c
@@ -0,0 +1,14 @@
+#include<stdio.h> 
+int main(){
+           int n,n1,r,i;
+           printf("   Give the First number for the Range :   \n");
+           scanf("%d",&n);
+           printf("   Give the Final number for the Range :   \n");
+           scanf("%d",&n1);
+           printf("\n The Even numbers between %d and %d are ",f1,f2); 
+           for(i=n; i<=n1; ++i){
+               r=((i%2)==0)
+               printf("\n  %d",i);
+           }
+    return 0; 
+    } 
