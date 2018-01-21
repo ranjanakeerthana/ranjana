5b2f90e03540864edda45e7c35bc626b96d7cabc
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-01-21 15:38:20 +0530

    Create 33.c

---
+++ set4/33.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+      char str [81];
+      int  nspace;
+      int i;
+      clrscr();
+      printf("Enter a line of text:\n");
+      gets(str);
+        nspace = 0; 
+      i = 0;
+      while (str[i] != '\0')
+          {
+          
+               if (ch == ' ' || ch =='\n' || ch == '\t')
+                       nspace++;
+          
+          }
+           
+              printf("White spaces : %d",  nspace);
+        
+              getch();
+} 
