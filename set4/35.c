2f6de7b975a5f8adedc16917b04f39bcf86db1a5
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-01-21 15:55:57 +0530

    Create 35.c

---
+++ set4/35.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+   char s[100];
+   int i = 0, l = 0, f = 1;
+   clrscr();
+   puts("Enter any string\n");
+   gets(s);
+   for(i = 0; s[i] !='\0'; i++)
+   {
+      l = l + 1;
+   }
+   printf("The number of characters in the string are %d\n", l);
+   getch();
+}
