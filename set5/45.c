bc419d41b376b4947ed950d6afd45b3578cabd95
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-01-22 19:39:01 +0530

    Create 45.c

---
+++ set5/45.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,m,j;
+printf("enter the numbers:");
+scanf("%d%d",&n,&m);
+printf("enter the number to check whether it is within the ranga are not");
+scanf("%d",j);
+if(j<=n||j>=m);
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+return 0;
+}
