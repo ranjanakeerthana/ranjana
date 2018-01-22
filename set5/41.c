8859e653be9caed0e7380e4b492641a92e7d77d9
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-01-22 19:01:28 +0530

    Create 41.c

---
+++ set5/41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+char s[10];
+printf("enter the value and string");
+scanf("%d %s",&n,&s);
+for(i=0;i<n;i++)
+{
+printf("%s",s);
+}
+return 0;
+getch();
+}
