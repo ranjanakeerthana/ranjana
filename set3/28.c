77f35ab642d27433a44b9e7f8259e63fb6bb863e
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-02-02 15:00:05 +0530

    Create 28.c

---
+++ set3/28.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,a[10];
+printf("enter the array size");
+scanf("%d",&n);
+for(i=0;i<=k;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=k;i++)
+{
+printf("%d %d",i,a[i]);
+}
+}
