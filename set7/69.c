a032f4512cd77a5a492ce54c23636fb44eb84cec
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-02-02 19:37:55 +0530

    Create 69.c

---
+++ set7/69.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,m;
+printf("enter two numbers:");
+scanf("%d %d",&n,&m);
+n=n-m;
+if(n%2==0)
+{
+printf("%d  is even",n);
+}
+else
+{
+printf("%d is odd",n);
+}
+}
+© 2018 GitHub, Inc.
