86addeb91db08fc9cb1147b749818f4cb803faea
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-02-11 14:40:38 +0530

    Create 91.c

---
+++ set10/91.c
@@ -0,0 +1,10 @@
+#innclude<stdio.h>
+int main()
+{
+int length,width,height,volume;
+printf(enter length,width,height of cuboid");
+scanf("%d %d %d",&length,&width,&height);
+printf("the volume of cuboid");
+volume=length*width*height;
+printf("%d",volume);
+}
