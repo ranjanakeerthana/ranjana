3782618500a311f18de4ec37fd21f259a3051d39
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-02-28 13:14:59 +0200

    Update 75.c

--- set8/75.c
+++ set8/75.c
@@ -1,25 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<string.h>
-void main()
-{
-char a[10];
-int i,j;
-scanf("%s",a);
-i=strlen(a);
-j=i%2;
-for(k=0;a[k]!='\0';k++)
-{
-if(j==0)
-{
-printf("%c",&a[k]);
-}
-else if(j%2==1)
-{
-printf("**");
-}
-else
-{
-printf("*");
-}
-}
+int main(void) {
+	char a[10];
+	int b;
+	scanf("%s",a);
+	b=strlen(a);
+	if(b%2==0)
+	{
+		a[b/2]='*';
+		a[(b/2)-1]='*';
+	}
+	else
+	{
+		a[b/2]='*';
+	}
+	printf("%s",a);
+	return 0;
 }
