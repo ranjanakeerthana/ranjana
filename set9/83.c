06e2bae9258e2011e3113a56cd305ddd747b0738
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-03-02 15:47:42 +0530

    Update 83.c

--- set9/83.c
+++ set9/83.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 void main()
 {
-int x[3],y[3],remainder,cocient,i;
+int x[3],y[3],remainder,cocient,i,n;
+  printf("enter the n value");
+  scanf("%d",&n);
 printf("enter dividend and divisor");
-for(i=0;i<3;i++)
+for(i=0;i<n;i++)
 {
 scanf("%d%d\n",&x[i],&y[i]);
 }
 printf("the result of division is");
 {
-for(i=0;i<3;i++)
+for(i=0;i<n;i++)
 printf("%d\n",(x[i]/y[i]));
 }
 printf("the result of modulo is");
-for(i=0;i<3;i++)
+for(i=0;i<n;i++)
 {
 printf("%d\n",(x[i]%y[i]));
 }
