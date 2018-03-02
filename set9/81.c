c23e86876c318614a48d7c2e12d5b95d560039e4
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-03-02 15:41:29 +0530

    Create 81.c

---
+++ set9/81.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+ int kabali_ninja,opppnent_ninja,i,t;
+ for(i=0;i<3;i++)
+ {
+ scanf("%d %d",&kabali_ninja,&opppnent_ninja);
+ if(i==0)
+  printf("%d",opppnent_ninja-kabali_ninja);
+  if(i==1)
+  printf("%d",opppnent_ninja-kabali_ninja);
+  if(i==2)
+  printf("%d",opppnent_ninja-kabali_ninja);
+}
+return 0;
+}
