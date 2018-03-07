e42e33cdfb42c84b1ce706a974e998a92e4112e5
ranjanakeerthana <35601365+ranjanakeerthana@users.noreply.github.com>
2018-03-07 16:28:55 +0530

    Create 10.c

---
+++ player/set1/10.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+char a[10],b[10];
+int i,count=0;
+scanf("%s%s",a,b);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]!=b[i])
+{
+count++;
+}
+}
+if(count==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
